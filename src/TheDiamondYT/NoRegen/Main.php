88706c73fb43f43713047c629f274aa584b1369c
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-05-30 18:16:48 +0100

    Update Main.php

--- src/TheDiamondYT/NoRegen/Main.php
+++ src/TheDiamondYT/NoRegen/Main.php
@@ -23,23 +23,19 @@ class Main extends PluginBase implements Listener {
     }
     
     public function onRegainHealth(EntityRegainHealthEvent $ev) {
-        $block = $this->getConfig()->getAll()["block"];
         $reason = $ev->getRegainReason();
         $bypass = $ev->getPlayer()->hasPermission("noregen.bypass");
         
-        if($block["regen"] && $reason === EntityRegainHealthEvent::CAUSE_REGEN && !$bypass) {
+        if($reason === EntityRegainHealthEvent::CAUSE_REGEN && !$bypass) {
             $ev->setCancelled(true);
         }
-        if($block["eating"] && $reason === EntityRegainHealthEvent::CAUSE_EATING && !$bypass) {
+        if($reason === EntityRegainHealthEvent::CAUSE_MAGIC && !$bypass) {
             $ev->setCancelled(true);
         }
-        if($block["potion"] && $reason === EntityRegainHealthEvent::CAUSE_MAGIC && !$bypass) {
+        if($reason === EntityRegainHealthEvent::CAUSE_CUSTOM && !$bypass) {
             $ev->setCancelled(true);
         }
-        if($block["custom"] && $reason === EntityRegainHealthEvent::CAUSE_CUSTOM && !$bypass) {
-            $ev->setCancelled(true);
-        }
-        if($block["saturation"] && $reason === EntityRegainHealthEvent::CAUSE_SATURATION && !$bypass) {
+        if($reason === EntityRegainHealthEvent::CAUSE_SATURATION && !$bypass) {
             $ev->setCancelled(true);
         }
     }
